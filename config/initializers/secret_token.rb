774a07afbaa6fba822e9b4b103225f3393917ab0
Ben Shymkiw <ben@t.apio.ca>
2010-12-06 15:31:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrankAppsTest::Application.config.secret_token = 'a05cb66a0616ec7a487b33891e7dd3de1e5bc9776a6318eba3d885e6fd312d5ab8051f892eba39cfecc68e63fa08ad5fba2da668126820a473929f1eea48f4e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CrankAppsTest::Application.config.session_store :cookie_store, :key => '_crank_apps_test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CrankAppsTest::Application.config.session_store :active_record_store
